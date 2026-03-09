55472ffeec1c44bc0757e0a5d83edda4ce2b5bb2
William Sando <will.sando@waracle.com>
2026-03-09 16:30:06 +0000

    BOOK_SKYE --> SKYE_HOSTS

--- apps/skye-glamping-website/setup-jest.mjs
+++ apps/skye-glamping-website/setup-jest.mjs
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
 process.env.NODE_ENV = 'test';
-process.env.NEXT_PUBLIC_BOOK_SKYE_API_URL = 'http://localhost:3003';
+process.env.NEXT_PUBLIC_SKYE_GLAMPING_API_URL = 'http://localhost:3003';
 process.env.NEXTAUTH_SECRET = 'test-secret';
 process.env.NEXT_PUBLIC_GLAMPING_WEBSITE_SENTRY_DSN = '';
--- packages/book-skye-api-client/src/get-api-base-url.ts
+++ packages/book-skye-api-client/src/get-api-base-url.ts
@@ -1,8 +1,8 @@
 export function getApiBaseUrl(): string {
-  const url = process.env.NEXT_PUBLIC_BOOK_SKYE_API_URL;
+  const url = process.env.NEXT_PUBLIC_SKYE_GLAMPING_API_URL;
   if (!url) {
     throw new Error(
-      'NEXT_PUBLIC_BOOK_SKYE_API_URL environment variable is not set. ' +
+      'NEXT_PUBLIC_SKYE_GLAMPING_API_URL environment variable is not set. ' +
         'Please set it to the API base URL (e.g. https://api.bookskye.co.uk).',
     );
   }
