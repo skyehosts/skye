c6cf0e5a8c580c6659c4fed99fced5f8cf6f552e
William Sando <will.sando@waracle.com>
2026-03-11 16:33:57 +0000

    principle column

--- apps/skye-hosts-guest-website/app/contact/page.tsx
+++ apps/skye-hosts-guest-website/app/contact/page.tsx
@@ -1,5 +1,13 @@
 import { HeadingPanel } from '@repo/web-components/layout/heading-panel';
+import { PrincipalColumn } from '@repo/web-components/layout/principal-column';
 
 export default function ContactPage() {
-  return <HeadingPanel title="Contact" />;
+  return (
+    <>
+      <HeadingPanel title="Contact" />
+      <PrincipalColumn>
+        <p>foo</p>
+      </PrincipalColumn>
+    </>
+  );
 }
---
+++ packages/web-components/src/layout/principal-column.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { Box, type BoxProps } from '@mui/material';
+import { type ReactNode } from 'react';
+
+interface PrincipalColumnProps {
+  children: ReactNode;
+  sx?: BoxProps['sx'];
+}
+
+export function PrincipalColumn({ children, sx }: PrincipalColumnProps) {
+  return (
+    <Box
+      sx={[
+        {
+          mx: 'auto',
+          textAlign: 'center',
+          justifyContent: 'center',
+          display: 'flex',
+          flexDirection: 'column',
+          fontSize: {
+            xs: '1rem',
+            md: '1.0625rem',
+            lg: '1.125rem',
+            xl: '1.1875rem',
+          },
+          width: {
+            xs: '100%',
+            md: '75%',
+            lg: '65%',
+            xl: '50%',
+          },
+        },
+        ...(Array.isArray(sx) ? sx : sx ? [sx] : []),
+      ]}
+    >
+      {children}
+    </Box>
+  );
+}
